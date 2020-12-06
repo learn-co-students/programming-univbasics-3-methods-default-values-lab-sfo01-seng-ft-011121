8bea71171dcdc2d367929e5bb81db3556652a817
Erik Huang <erikhuangdrums@gmail.com>
2020-12-06 00:42:08 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  fuck_you_string = "A plate of #{protein} with #{veg1} and #{veg2}."
-  puts fuck_you_string
-  return fuck_you_string
+  string = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts string
+  return string
 end
